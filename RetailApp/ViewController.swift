cdf4806fa40df8d0f1e299b6f8ff2efda4040914
Umurcan Kocaman <umurcan.kocaman@ing.com.tr>
2021-11-01 01:41:13 +0300

    Initial Commit

---
+++ RetailApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetailApp
+//
+//  Created by Umurcan Kocaman [Bireysel Mobil Bankacilik Squad 2] on 1.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
