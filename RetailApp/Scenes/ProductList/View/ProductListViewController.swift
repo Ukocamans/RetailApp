0953f18015563413476f34e9734f6028b6ccf5cf
Umurcan Kocaman <umurcan.kocaman@ing.com.tr>
2021-11-01 16:30:45 +0300

    Add Product Cell

--- RetailApp/Scenes/ProductList/View/ProductListViewController.swift
+++ RetailApp/Scenes/ProductList/View/ProductListViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
-class ProductListViewController: UIViewController {
+final class ProductListViewController: UIViewController {
 
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        collectionView.register(ProductCell.self)
+        collectionView.dataSource = self
+    }
+}
+
+extension ProductListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ProductCell = collectionView.dequeueReusableCell(for: indexPath)
+        return cell
     }
 }
